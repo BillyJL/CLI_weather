ce173c0dbd754b84bff2cd1351edc6c770f12e4c
Taras <tarasmikulskyi@ukr.net>
2023-01-05 11:29:19 +0200

    Added the function of creating/updating the configuration file in which the token is stored

--- weather.js
+++ weather.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+	if (!token.length) {
+		printError('Token not entered');
+		return;
+	}
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Token saved');
+	} catch (e) {
+		printError(e.message)
+	}
+}
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
-	console.log(args);
 	if (args.h) {
 		printHelp();
 	}
@@ -12,7 +25,7 @@ const initCLI = () => {
 		// save city
 	}
 	if (args.t) {
-		// save token
+		return saveToken(args.t)
 	}
 	// output weather report
 };
